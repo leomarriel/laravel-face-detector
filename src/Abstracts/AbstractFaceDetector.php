2b4a9e862aecccf8fd77dba4d876aa4c6e744c65
Christin Gabriel <trampish@gmail.com>
2023-09-01 03:14:53 -0400

    Additional typecasting

--- src/Abstracts/AbstractFaceDetector.php
+++ src/Abstracts/AbstractFaceDetector.php
@@ -6,7 +6,7 @@ use Leomarriel\FaceDetector\Contracts\FaceDetectorContract;
 use Leomarriel\FaceDetector\Exceptions\NotFoundFaceException;
 
 abstract class AbstractFaceDetector implements FaceDetectorContract
-{   
+{
     /** @var string */
     private $reduced_canvas;
 
@@ -21,20 +21,20 @@ abstract class AbstractFaceDetector implements FaceDetectorContract
     * @return array|false
     */
     public function detector($target)
-    {    
-        $im_width = imagesx($target);
-        $im_height = imagesy($target);
+    {
+        $im_width = (int) imagesx($target);
+        $im_height = (int) imagesy($target);
 
         $diff_width = 320 - $im_width;
         $diff_height = 240 - $im_height;
         if ($diff_width > $diff_height) {
-            $ratio = $im_width / 320;
+            $ratio = (int) ($im_width / 320);
         } else {
-            $ratio = $im_height / 240;
+            $ratio = (int) ($im_height / 240);
         }
 
         if ($ratio != 0) {
-            $this->reduced_canvas = imagecreatetruecolor($im_width / $ratio, $im_height / $ratio);
+            $this->reduced_canvas = imagecreatetruecolor(intval($im_width / $ratio), intval($im_height / $ratio));
 
             imagecopyresampled(
                 $this->reduced_canvas,
@@ -43,8 +43,8 @@ abstract class AbstractFaceDetector implements FaceDetectorContract
                 0,
                 0,
                 0,
-                $im_width / $ratio,
-                $im_height / $ratio,
+                intval($im_width / $ratio),
+                intval($im_height / $ratio),
                 $im_width,
                 $im_height
             );
@@ -75,7 +75,7 @@ abstract class AbstractFaceDetector implements FaceDetectorContract
         }
 
         if($this->face && $this->face['w'] ?? 0 > 0) {
-            return $this->face; 
+            return $this->face;
         }
 
         if($this->settings['noFoundFaceException']) {
@@ -86,7 +86,7 @@ abstract class AbstractFaceDetector implements FaceDetectorContract
 
         return false;
     }
-    
+
     abstract protected function getImgStats($tmpFile);
 
     abstract protected function computeII($tmpFile, $image_width, $image_height);
@@ -94,5 +94,5 @@ abstract class AbstractFaceDetector implements FaceDetectorContract
     abstract protected function doDetectGreedyBigToSmall($ii, $ii2, $width, $height);
 
     abstract protected function detectOnSubImage($x, $y, $scale, $ii, $ii2, $w, $iiw, $inv_area);
-    
+
 }
